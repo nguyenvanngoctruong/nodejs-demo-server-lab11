32492693065864617680216da6a9228d0ce89e70
Truong-VAIO\Truong <nguyenvanngoctruong@gmail.com>
2018-06-20 23:24:10 +0700

    sasa
    asas

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var AWS = require('aws-sdk');
 */
 // Set your region
 AWS.config.region = 'us-east-2c';
-var db = new AWS.DynamoDB();
-db.listTables(function(err, data) {
- console.log(data.TableNames);
+var dynamodb = new AWS.DynamoDB();
+dynamodb.batchGetItem(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
 });
