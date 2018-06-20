69746219db59b0596fc5432ec8497607b78a0121
Truong-VAIO\Truong <nguyenvanngoctruong@gmail.com>
2018-06-20 23:58:20 +0700

    c1
    c1

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ var AWS = require('aws-sdk');
 * Create an IAM role for your EC2 instance instead.
 */
 // Set your region
-AWS.config.region = 'us-east-1';
+AWS.config.region = 'us-east-2';
 var db = new AWS.DynamoDB();
 db.listTables(function(err, data) {
  console.log(data.TableNames);
+ downloadData();
 });
+
+function downloadData(){
+ // Get JSON file from S3
+ var s3 = new AWS.S3();
+var params = {Bucket: 'nodejs-bucket-demo', Key: 'lab-data/test-table-items.json'};
+ s3.getObject(params, function(error, data) {
+ if (error) {
+ console.log(error); // error is Response.error
+ } else {
+ var dataJSON = JSON.parse(data.Body);
+ console.log(JSON.stringify(dataJSON) ) ;
+ }
+ });
+};
