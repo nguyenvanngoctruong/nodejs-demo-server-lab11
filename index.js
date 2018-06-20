57ef7a7ee78e111b8d1bbcc0c607b76af6237a93
Truong-VAIO\Truong <nguyenvanngoctruong@gmail.com>
2018-06-20 23:37:26 +0700

    das
    das

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 /**
 * Don't hard-code your credentials!
 * Create an IAM role for your EC2 instance instead.
 */
 // Set your region
-AWS.config.region = 'us-east-2c';
-var dynamodb = new AWS.DynamoDB();
-dynamodb.batchGetItem(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
+AWS.config.region = 'us-east-1';
+var db = new AWS.DynamoDB();
+db.listTables(function(err, data) {
+ console.log(data.TableNames);
 });
